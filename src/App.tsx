7e24a5da0b1a00bba04fd020d67bdbe99ab7a187
Matthew Gunderson <matthew.gunderson12@gmail.com>
2022-11-02 19:27:21 -0700

    Updated Landing page, added minimal styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Grid } from '@mui/material';
+import { textAlign } from '@mui/system';
 import './App.css';
-import Landing from './Componets/Landing';
+import Landing from './Componets/Views/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello world</p>
-      <Landing />
+      <Grid container spacing={2}>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <h3 style={{ textAlignLast: 'center' }}>Worship Song Select</h3>
+          <Landing />
+        </Grid>
+        <Grid item xs={4}></Grid>
+      </Grid>
+
+    
+
     </div>
   );
 }
